6d9c02f47e0484e5d5638bed7829bc7940b88e95
runzen <55329832+run-zen@users.noreply.github.com>
2021-04-10 10:17:32 +0530

    second commit

--- js/scripts.js
+++ js/scripts.js
@@ -1,35 +1,23 @@
-$(document).ready(function() {
-     $("#mycarousel").carousel({interval : 2000});
-     $("#carouselButton").click(function() {
-         if ($("#carouselButton").children('span').hasClass('fa-pause'))
-         {
-             $("#mycarousel").carousel('pause');
-             $("#carouselButton").children('span').removeClass('fa-pause');
-             $("#carouselButton").children('span').addClass('fa-play');
-         }
-         else if ($("#carouselButton").children('span').hasClass('fa-play'))
-         {
-             $("#mycarousel").carousel('cycle');
-             $("#carouselButton").children('span').removeClass('fa-play');
-             $("#carouselButton").children('span').addClass('fa-pause');
-         }
-     });
-     $("#loginButton").click(function() {
-         $("#loginmodal").modal('show');
-     });
-     $("#loginModalClose").click(function() {
-         $('#loginmodal').modal('hide');
-     });
-     $("#loginCancel").click(function() {
-         $('#loginmodal').modal('hide');
-     });
-     $("#reserveButton").click(function() {
-         $('#reservationmodal').modal('show');
-     });
-     $("#reserveModalClose").click(function() {
-         $('#reservationmodal').modal('hide');
-     });
-     $("#reserveCancel").click(function() {
-         $('#reservationmodal').modal('hide');
-     });
- });
+$(document).ready(function () {
+  $("#mycarousel").carousel({ interval: 2000 });
+  $("#carouselButton").click(function () {
+    if ($("#carouselButton").children("span").hasClass("fa-pause")) {
+      $("#mycarousel").carousel("pause");
+      $("#carouselButton").children("span").removeClass("fa-pause");
+      $("#carouselButton").children("span").addClass("fa-play");
+    } else if ($("#carouselButton").children("span").hasClass("fa-play")) {
+      $("#mycarousel").carousel("cycle");
+      $("#carouselButton").children("span").removeClass("fa-play");
+      $("#carouselButton").children("span").addClass("fa-pause");
+    }
+  });
+  $("#reserveButton").click(function () {
+    $("#reservationmodal").modal("show");
+  });
+  $("#reserveModalClose").click(function () {
+    $("#reservationmodal").modal("hide");
+  });
+  $("#reserveCancel").click(function () {
+    $("#reservationmodal").modal("hide");
+  });
+});
